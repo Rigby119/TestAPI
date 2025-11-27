d8f6e95caf62685fdfd24911e8837b48e5de5f53
Rigby119 <rigby119@gmail.com>
2025-11-27 09:37:45 -0600

    [ADD] Navigation + Main Activity call

--- app/src/main/java/com/app/testapi/MainActivity.kt
+++ app/src/main/java/com/app/testapi/MainActivity.kt
@@ -11,40 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.app.testapi.presentation.navigation.AppNavGraph
 import com.app.testapi.presentation.theme.TestAPITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TestAPITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavGraph(navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestAPITheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/app/testapi/presentation/navigation/NavGraph.kt
@@ -0,0 +1,20 @@
+package com.app.testapi.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.app.testapi.presentation.screens.HomeScreen
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+    ) {
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
